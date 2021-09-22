8b9dd1134918b8f780d97ac733bf771dc6fedaf7
che36 <tron36.cc@gmail.com>
2021-09-22 15:31:31 -0300

    Added Rest

---
+++ src/main/java/br/com/constanzo/livecodingcampeonatobrasileiro/rest/JogoRestController.java
@@ -0,0 +1,57 @@
+package br.com.constanzo.livecodingcampeonatobrasileiro.rest;
+
+import br.com.constanzo.livecodingcampeonatobrasileiro.dto.ClassificacaoDTO;
+import br.com.constanzo.livecodingcampeonatobrasileiro.dto.JogoDTO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/jogos")
+public class JogoRestController {
+
+    @Autowired
+    private JogoService jogoService;
+
+    @ApiOperation(value = "Gera os jogos do campeonato!")
+    @PostMapping
+    public ResponseEntity<Void> gerarJogos() {
+        jogoService.gerarJogos(LocalDateTime.now(), Arrays.asList());
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation(value = "Retorna um jogo específico")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<JogoDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(jogoService.findById(id));
+    }
+
+    @ApiOperation(value = "Retorna todos os jogos")
+    @GetMapping
+    public ResponseEntity<List<JogoDTO>> findAll() {
+        return ResponseEntity.ok().body(jogoService.findAll());
+    }
+
+    @ApiOperation(value = "Retorna a classificação")
+    @GetMapping(value = "/classificacao")
+    public ResponseEntity<ClassificacaoDTO> getClassificacao() {
+        return ResponseEntity.ok().body(jogoService.getClassificacao());
+    }
+
+    @ApiOperation(value = "Finaliza um jogo")
+    @PostMapping(value = "/finalizar/{id}")
+    public ResponseEntity<Void> finalizarJogo(@PathVariable Integer id, @RequestBody JogoDTO jogoDTO) throws Exception {
+        jogoService.finalizarJogo(id, jogoDTO);
+        return ResponseEntity.ok().build();
+    }
+}
