741e35af3f658e9cf54dbd6e76733501ac2e6b9e
che36 <tron36.cc@gmail.com>
2021-09-22 14:18:49 -0300

    Initial Setup

---
+++ src/main/java/br/com/constanzo/livecodingcampeonatobrasileiro/LivecodingCampeonatoBrasileiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.constanzo.livecodingcampeonatobrasileiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecodingCampeonatoBrasileiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivecodingCampeonatoBrasileiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/constanzo/livecodingcampeonatobrasileiro/LivecodingCampeonatoBrasileiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.constanzo.livecodingcampeonatobrasileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecodingCampeonatoBrasileiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
